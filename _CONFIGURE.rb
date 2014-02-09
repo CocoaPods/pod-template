b63fed396b8b7eb7574b033cfecfd42948b8e269
André Schneider <hello@andreschneider.me>
2014-02-09 16:26:12 +0100

    Changed the template folder structure to match CocoaPods „Making a CocoaPod Guide“.

--- _CONFIGURE.rb
+++ _CONFIGURE.rb
@@ -31,7 +31,7 @@ module Pod
     end
 
     def replace_variables_in_files
-      file_names = ['LICENSE', 'POD_README.md', 'CHANGELOG.md', 'NAME.podspec', 'Project/Podfile']
+      file_names = ['LICENSE', 'POD_README.md', 'CHANGELOG.md', 'NAME.podspec', 'Example/Podfile']
       file_names.each do |file_name|
         text = File.read(file_name)
         text.gsub!("${POD_NAME}", pod_name)
