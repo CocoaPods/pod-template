1b77af09c1170b3134a26f1cb211fdb5b146eaf9
Orta <orta.therox@gmail.com>
2014-06-21 13:57:38 -0400

    remove the host bundle reference

--- setup/MessageBank.rb
+++ setup/MessageBank.rb
@@ -17,16 +17,16 @@ module Pod
     def green_bang
       "! ".green
     end
-    
+
     def red_bang
       "! ".red
     end
-    
+
     def run_command command
       puts "  " + command.magenta
       system command
     end
-    
+
 
     def welcome_message
       unless @configurator.validate_user_details
@@ -43,27 +43,27 @@ module Pod
 
 
     def run_setup_questions
-      
+
       puts yellow_bang + "Before you can create a new library we need to setup your git credentials."
 
       unless @configurator.user_name.length > 0
-        puts "\n What is your name? "=
+        puts "\n What is your name? "
         answer = ""
 
         loop do
           show_prompt
-          
+
           answer = gets.downcase.chomp
           break if answer.length > 0
 
           puts red_bang + "Please enter a name."
         end
-        
+
         puts ""
         puts green_bang + "Setting your name in git to " + answer
         run_command('git config --global user.name "' + answer + '"')
       end
-      
+
       unless @configurator.user_email.length > 0
         puts "\n What is your email?"
         answer = ""
@@ -75,12 +75,12 @@ module Pod
 
           puts red_bang + "Please enter a email."
         end
-        
+
         puts ""
         puts green_bang + "Setting your email in git to " + answer
-        run_command('git config --global user.email "' + answer + '"')        
+        run_command('git config --global user.email "' + answer + '"')
       end
-      
+
     end
 
   end
--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -18,33 +18,41 @@ module Pod
 
     def run
       remove_demo_project if @remove_demo_target
-      
+
       @string_replacements = {
         "PROJECT_OWNER" => @configurator.user_name,
         "TODAYS_DATE" => @configurator.date,
         "PROJECT" => @configurator.pod_name,
       }
-      
+
       replace_internal_project_settings
       rename_files
       rename_project_folder
     end
-    
+
     def remove_demo_project
       project  = Xcodeproj::Project.open @xcodeproj_path
       app_project = project.targets.select { |target| target.product_type == "com.apple.product-type.application" }.first
       test_target = project.targets.select { |target| target.product_type == "com.apple.product-type.bundle.unit-test" }.first
-      
+
+      # Remove the implicit dependency on the app
       test_dependency = test_target.dependencies.first
       test_dependency.remove_from_project
       app_project.remove_from_project
+
+      # remove the build target on the unit tests
+      test_target.build_configuration_list.build_configurations.each do |build_config|
+        build_config.build_settings.delete "BUNDLE_LOADER"
+      end
+
+
       project.save
     end
-    
+
     def project_folder
       File.dirname @xcodeproj_path
     end
-    
+
     def rename_files
       File.rename(project_folder + "/PROJECT.xcodeproj", project_folder + "/" +  @configurator.pod_name + ".xcodeproj")
 
@@ -52,26 +60,26 @@ module Pod
         before = project_folder + "/PROJECT/" + file
         after = project_folder + "/PROJECT/" + file.gsub("PROJECT", @configurator.pod_name)
         File.rename before, after
-      end      
+      end
     end
-    
+
     def rename_project_folder
       File.rename(project_folder + "/PROJECT", project_folder + "/" + @configurator.pod_name)
     end
-    
-    def replace_internal_project_settings      
+
+    def replace_internal_project_settings
       Dir.glob(project_folder + "/*/*").each do |name|
         next if Dir.exists? name
         text = File.read(name)
-  
+
         for find, replace in @string_replacements
             text = text.gsub(find, replace)
         end
-  
+
         File.open(name, "w") { |file| file.puts text }
       end
     end
-    
+
   end
-  
+
 end
