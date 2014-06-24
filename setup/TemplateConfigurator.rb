298cca3f38f26cd982e6cce3a0929708bec03b9f
Orta <orta.therox@gmail.com>
2014-06-23 20:49:30 -0400

    improve question function

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -13,17 +13,39 @@ module Pod
     end
 
     def ask_with_answers(question, possible_answers)
-      possible_answers_string = possible_answers.join(" ")
-      puts ""
-      puts " #{question}? [#{possible_answers_string}]"
+
+      print "\n#{question}? ["
+
+      print_info = Proc.new {
+
+        possible_answers_string = possible_answers.each_with_index do |answer, i|
+           _answer = (i == 0) ? answer.underline : answer
+           print " " + _answer
+           print(" /") if i != possible_answers.length-1
+        end
+        print " ]\n"
+      }
+      print_info.call
+
       answer = ""
 
       loop do
         @message_bank.show_prompt
         answer = gets.downcase.chomp
+
+        answer = "yes" if answer == "y"
+        answer = "no" if answer == "n"
+
+        # default to first answer
+        if answer == ""
+          answer = possible_answers[0].downcase
+          print answer.yellow
+        end
+
         break if possible_answers.map { |a| a.downcase }.include? answer
 
-        puts "Possible answers: #{possible_answers_string}"
+        print "\nPossible answers are ["
+        print_info.call
       end
 
       answer
