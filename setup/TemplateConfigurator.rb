5e35f51d2a7934bb44c2b091b48694df77139f4d
Kyle Fuller <inbox@kylefuller.co.uk>
2014-07-07 19:19:16 +0100

    Fixes issue with podname using `+` would break travis badges
    Related to #56

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -104,6 +104,7 @@ module Pod
       file_names.each do |file_name|
         text = File.read(file_name)
         text.gsub!("${POD_NAME}", @pod_name)
+        text.gsub!("${REPO_NAME}", @pod_name.gsub('+', '-'))
         text.gsub!("${USER_NAME}", user_name)
         text.gsub!("${USER_EMAIL}", user_email)
         text.gsub!("${YEAR}", year)
