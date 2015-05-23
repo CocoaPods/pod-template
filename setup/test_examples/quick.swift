7d5c5c208453780d70f307387a88f4e970f89a56
Orta Therox <orta.therox@gmail.com>
2015-05-23 11:57:55 +0100

    auto import user project in swift, fixes module mapping in objc

--- setup/test_examples/quick.swift
+++ setup/test_examples/quick.swift
@@ -2,6 +2,7 @@
 
 import Quick
 import Nimble
+import PROJECT
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
--- setup/test_examples/xctest.swift
+++ setup/test_examples/xctest.swift
@@ -1,5 +1,6 @@
 import UIKit
 import XCTest
+import PROJECT
 
 class Tests: XCTestCase {
     
