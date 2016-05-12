bb8c7c0b500313f1342a9dc404040738d1fa3369
William Entriken <github.com@phor.net>
2016-05-12 19:43:12 -0400

    add spec for basic configuration

--- spec/default/basic_spec.rb
+++ spec/default/basic_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
-describe "Swift Integration" do
+describe "Idiot Mode" do
   before(:each) do
     FileUtils.rm_rf 'spec/staging'
     FileUtils::mkdir_p 'spec/staging'
   end
 
-  it "should work if you select swift and keep hitting enter" do
+  it "should work if you just keep hitting enter" do
     FileUtils::mkdir_p 'spec/staging'
     Dir.chdir('spec/staging') do
       puts "Vendoring with default settings"
       path = Dir.pwd + '/../mock:' + ENV['PATH']
       command = "bundle exec pod lib create --template-url='file://#{Dir.pwd}/../../' TestPod"
       Open3.popen3({'PATH' => path}, command) {|stdin, stdout, stderr, wait_thr|
-        stdin.write "swift\n\n\n\n\n\n\n"
+        stdin.write "\n\n\n\n\n\n\n"
         print stdout.readlines.join {"\n"}
         print stderr.readlines.join {"\n"}
       }
