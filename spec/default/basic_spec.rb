e4fa5b5de5c2bf2adc1121bf53331f6c937fb20e
William Entriken <github.com@phor.net>
2016-05-19 22:18:58 -0400

    answer questions

--- spec/default/basic_spec.rb
+++ spec/default/basic_spec.rb
@@ -9,11 +9,11 @@ describe "Idiot Mode" do
   it "should work if you just keep hitting enter" do
     FileUtils::mkdir_p 'spec/staging'
     Dir.chdir('spec/staging') do
-      puts "Vendoring with default settings"
+      puts "Vendoring with simple settings"
       path = Dir.pwd + '/../mock:' + ENV['PATH']
       command = "bundle exec pod lib create --verbose --template-url='file://#{Dir.pwd}/../../' TestPod"
       Open3.popen2e({'PATH' => path}, command) { |stdin, stdout_and_stderr, wait_thr|
-        stdin.write "\n\n\n\n\n\n\n"
+        stdin.write "Bob Smith\nbob.smith@example.com\n\n\n\n\n\n"
         stdin.close
         print stdout_and_stderr.readlines.join {"\n"}
       }
