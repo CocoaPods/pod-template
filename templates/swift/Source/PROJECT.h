a086c03248c99b45684ef62586d2ee99ee9b0f27
William Entriken <github.com@phor.net>
2016-08-01 23:57:05 -0700

    Add PROJECT.h file like AlamoFire

---
+++ templates/swift/Source/PROJECT.h
@@ -0,0 +1,12 @@
+//
+//  ${POD_NAME}.swift
+//  ${POD_NAME}
+//
+//  Created by ${USER_NAME} on ${DATE}.
+//  Copyright © ${YEAR} ${USER_NAME}. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double ${POD_NAME}VersionNumber;
+FOUNDATION_EXPORT const unsigned char ${POD_NAME}VersionString[];
