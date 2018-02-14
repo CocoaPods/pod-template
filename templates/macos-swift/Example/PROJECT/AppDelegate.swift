ae54fb07f86f6a52eb6d9ab7511feeeae8bfdfbc
Koji Murata <koji.murata@dena.com>
2018-02-15 00:26:15 +0900

    add test project

---
+++ templates/macos-swift/Example/PROJECT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PROJECT
+//
+//  Created by Koji Murata on 2018/02/15.
+//  Copyright © 2018年 Koji Murata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ templates/macos-swift/Example/PROJECT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PROJECT
+//
+//  Created by Koji Murata on 2018/02/15.
+//  Copyright © 2018年 Koji Murata. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
