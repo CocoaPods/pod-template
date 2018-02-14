1728d72247b43d9c0669e52d858fb9cac911264c
Koji Murata <koji.murata@dena.com>
2018-02-15 01:06:59 +0900

    remove podspec for osx

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -29,7 +29,7 @@ module Pod
       case snapshots
         when :yes
           configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
-          
+
           if keep_demo == :no
               puts " Putting demo application back in, you cannot do view tests without a host application."
               keep_demo = :yes
@@ -49,15 +49,18 @@ module Pod
       }).run
 
       `mv ./templates/swift/* ./`
-      
+
       # There has to be a single file in the Classes dir
       # or a framework won't be created
       `touch Pod/Classes/ReplaceMe.swift`
-      
-      # The Podspec should be 8.0 instead of 7.0      
+
+      # The Podspec should be 8.0 instead of 7.0
       text = File.read("NAME.podspec")
       text.gsub!("7.0", "8.0")
       File.open("NAME.podspec", "w") { |file| file.puts text }
+
+      # remove podspec for osx
+      `rm ./NAME-osx.podspec`
     end
   end
 
--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -71,12 +71,15 @@ module Pod
         :remove_demo_project => (keep_demo == :no),
         :prefix => prefix
       }).run
-      
+
       # There has to be a single file in the Classes dir
       # or a framework won't be created, which is now default
       `touch Pod/Classes/ReplaceMe.m`
 
       `mv ./templates/ios/* ./`
+
+      # remove podspec for osx
+      `rm ./NAME-osx.podspec`
     end
   end
 
