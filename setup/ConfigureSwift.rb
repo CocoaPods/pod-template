30f6e1d8ac4ee5c5d740cf65d1ce5f00afbc0375
SketchK <zhangsiqi1988@gmail.com>
2020-12-03 21:38:46 +0800

    Fix iOS version number not equal between Podfile and Podspec
    iOS version number is not equal between Podfile and Podspec
    This will cause error like this:
    ```
    [!] CocoaPods could not find compatible versions for pod "Test":
      In Podfile:
        Test (from `../`)
    
    Specs satisfying the `Test (from `../`)` dependency were found, but they required a higher minimum deployment target.
    ```

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -48,16 +48,11 @@ module Pod
         :prefix => ""
       }).run
 
-      `mv ./templates/swift/* ./`
-
       # There has to be a single file in the Classes dir
       # or a framework won't be created
       `touch Pod/Classes/ReplaceMe.swift`
 
-      # The Podspec should be 8.0 instead of 7.0
-      text = File.read("NAME.podspec")
-      text.gsub!("7.0", "8.0")
-      File.open("NAME.podspec", "w") { |file| file.puts text }
+      `mv ./templates/swift/* ./`
 
       # remove podspec for osx
       `rm ./NAME-osx.podspec`
